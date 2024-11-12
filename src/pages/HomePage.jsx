baf7d51f1c4d754feb18ac7024713ee38c129e2a
Omprakash Mourya. <105220456+Omprakashmourya143@users.noreply.github.com>
2024-11-12 11:19:06 +0530

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { fetchMovies } from '../services/api';
 import MovieList from '../components/MovieList';
 import Favorites from '../components/Favorites';
